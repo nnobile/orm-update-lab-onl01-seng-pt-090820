b0587b379c57db4284bf2cedc0cd92c157cb8731
Nick Nobile <nickcnobile@gmail.com>
2020-11-15 11:36:06 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,13 +5,12 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :name, :grade, :id
   
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end
 
   def self.create_table
@@ -34,32 +33,35 @@ class Student
   end
 
   def save
-    sql = <<-SQL
+    if self.id
+      self.update
+    else
+      sql =  <<-SQL
       INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      VALUES (?,?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
 
   def self.new_from_db(row)
-      new_student = self.new  # self.new is the same as running Song.new
-      new_student.id = row[0]
-      new_student.name =  row[1]
-      new_student.grade = row[2]
-      new_student
+    new_student = self.new(row[0], row[1], row[2])
+    new_student
   end
 
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
       WHERE name = ?
       LIMIT 1
     SQL
@@ -68,12 +70,10 @@ class Student
       self.new_from_db(row)
     end.first
   end
-
+ 
   def update
-    # UPDATE students
-    # SET column1 = value1, column2 = value2
-    # WHERE condition;
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-
 
 end
